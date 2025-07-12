5667f3aafe63bee1377d33c6622edbb0e8f0df88
mtess11 <mariya.teslya@live.com>
2025-07-11 20:26:50 -0400

    Added rest framework into settings.py

--- medications/models.py
+++ medications/models.py
@@ -4,6 +4,5 @@ class Medication(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=50)
     manufacturer = models.CharField(max_length=255, blank=True, null=True)
-
     def __str__(self):
         return self.name
