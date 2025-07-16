ccfc3a297b3eab804f99810126a7b559402fdd10
mtess11 <mariya.teslya@live.com>
2025-07-16 18:57:25 -0400

    fixed bug in the create medication fx

--- medications/schema.py
+++ medications/schema.py
@@ -1,3 +1,4 @@
+from django.db.models import ObjectDoesNotExist
 import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Medication
@@ -10,13 +11,13 @@ class Query(graphene.ObjectType):
     medications = graphene.List(MedicationType)
     medication = graphene.Field(MedicationType, id=graphene.Int())
 
-    def resolve_medications(root, info, **kwargs):
+    def resolve_medications(root, info):
         return Medication.objects.all()
 
     def resolve_medication(root, info, id):
         try:
             return Medication.objects.get(pk=id)
-        except Medication.DoesNotExist:
+        except ObjectDoesNotExist:
             return None
 
 class CreateMedication(graphene.Mutation):
@@ -27,7 +28,8 @@ class CreateMedication(graphene.Mutation):
 
     medication = graphene.Field(MedicationType)
 
-    def mutate(self, info, name, code, manufacturer=None):
+    @staticmethod
+    def mutate(root, info, name, code, manufacturer=None):
         medication = Medication(name=name, code=code, manufacturer=manufacturer)
         medication.save()
         return CreateMedication(medication=medication)
